5be3aa84763472c62fbce72d492473927bf9e8fa
Osh <gusmunos@student.gu.se>
2021-12-05 21:07:24 +0100

    Lottie has installed and it is working :)

--- src/screens/Demo.js
+++ src/screens/Demo.js
@@ -2,9 +2,12 @@ import { Button, Text, View } from "react-native";
 import React from "react";
 import { useTheme } from "@react-navigation/native";
 import { GlobalStyles } from "../styles/global.styles";
+import LottieView from 'lottie-react-native';
 
 // this is for demo to prepare how font and themes should look like
 //TODO remove this when everything is finished after app.
+
+
 
 const Demo = () => {
   const { colors } = useTheme();
@@ -13,6 +16,7 @@ const Demo = () => {
       <Text style={colors.text}>Hello there!</Text>
       <Button title={"bajs"} color={colors.button.backgroundColor} />
       <Button title={"bajs"} color={colors.button.color} />
+      <LottieView style={{width: 500}} source={require('../../assets/lottie/manLottie.json')} autoPlay={true} />
     </View>
   );
 };
