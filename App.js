9091185917211afb3f65a834d9ffc056dac7ab5e
Osh <gusmunos@student.gu.se>
2021-12-05 17:10:21 +0100

    formatted the code and clean up

--- App.js
+++ App.js
@@ -5,13 +5,13 @@ import { GlobalStyles } from "./src/styles/global.styles";
 import { SafeAreaView, StatusBar } from "react-native";
 import { AppStyles } from "./src/styles/app.styles";
 
-const {safeAreaViewContainer} = AppStyles
+const { safeAreaViewContainer } = AppStyles;
 const App = () => {
   return (
     <NavigationContainer theme={GlobalStyles}>
       <StatusBar backgroundColor={"#d5c0ad"} barStyle={"dark-content"} />
       <SafeAreaView style={safeAreaViewContainer}>
-        <Demo/>
+        <Demo />
       </SafeAreaView>
 
     </NavigationContainer>
--- src/styles/app.styles.js
+++ src/styles/app.styles.js
@@ -1,9 +1,9 @@
 import { StyleSheet } from "react-native";
 
 export const AppStyles = StyleSheet.create({
-  safeAreaViewContainer:{
+  safeAreaViewContainer: {
     backgroundColor: "#F8E7D7",
     width: "100%",
-    height:"100%"
+    height: "100%",
   },
-})
+});
