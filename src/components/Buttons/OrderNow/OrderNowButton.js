7ae12bc7c21af4baa2ddd33c519ec156dabd72ac
Osh <gusmunos@student.gu.se>
2021-12-09 00:58:15 +0100

    Forgot to add shadow because of you git.

--- src/components/Buttons/OrderNow/OrderNowButton.js
+++ src/components/Buttons/OrderNow/OrderNowButton.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import { OrdernowbuttonStyles } from "./ordernowbutton.styles";
+import { AppStyles } from "../../../styles/app.styles";
 
 const { buttonContainer, textContainer } = OrdernowbuttonStyles;
+const {shadow} = AppStyles
 const OrderNowButton = () => {
   return (
     <View>
-      <TouchableOpacity style={buttonContainer}>
+      <TouchableOpacity style={[buttonContainer, shadow]}>
         <Text style={textContainer}>ORDER NOW</Text>
       </TouchableOpacity>
     </View>
