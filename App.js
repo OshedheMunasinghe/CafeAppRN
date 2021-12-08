50872cb51b54bf96c494c8f11dabc0719b9ef817
Osh <gusmunos@student.gu.se>
2021-12-08 16:30:06 +0100

    fixed bug better version with lottie in class version

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import Demo from "./src/screens/Demo";
 import { NavigationContainer } from "@react-navigation/native";
+import { AppStyles } from "./src/styles/app.styles";
 import { GlobalStyles } from "./src/styles/global.styles";
 import { SafeAreaView, StatusBar } from "react-native";
-import { AppStyles } from "./src/styles/app.styles";
+import Demo from "./src/screens/Demo";
+import ClassDemo from "./src/screens/ClassDemo";
 
 const { safeAreaViewContainer } = AppStyles;
+
 const App = () => {
   return (
     <NavigationContainer theme={GlobalStyles}>
       <StatusBar backgroundColor={"#d5c0ad"} barStyle={"dark-content"} />
       <SafeAreaView style={safeAreaViewContainer}>
-        <Demo />
+        {/*<ClassDemo/>*/}
+        <Demo/>
       </SafeAreaView>
-
     </NavigationContainer>
   );
 };
--- src/screens/Demo.js
+++ src/screens/Demo.js
@@ -1,5 +1,5 @@
 import { Button, Text, TouchableWithoutFeedback, View } from "react-native";
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import { useTheme } from "@react-navigation/native";
 import { GlobalStyles } from "../styles/global.styles";
 import LottieView from "lottie-react-native";
@@ -9,50 +9,58 @@ import LottieView from "lottie-react-native";
 
 // todo TRY NAVIGATION IF THAT IS WORKING TOO! AND THEN GO AND WORK!
 
-const Demo = () => {
-  const { colors } = useTheme();
+class Demo extends Component {
 
 
-  const [signal, setSignal] = useState(false); //signal is false
-
-  const playCoffee = () => {
-    console.log('playing coffee animation')
-    this.play_btn.play()
-    setTimeout(() => {  console.log("pausing...");  callResumeCoffee();}, 2500);
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      play_btn: false,
+    };
   }
+  // const { colors } = useTheme(); // ! THIS IS THE PROBLEM
 
-  const callResumeCoffee = () => {
-    console.log('playing outro')
-    this.play_btn.play(75,0)
+
+
+  render() {
+    // const { colors } = useTheme();
+    const playCoffee = () => {
+      console.log("playing coffee animation");
+      this.play_btn.play();
+      setTimeout(() => {
+        console.log("pausing...");
+        callResumeCoffee();
+      }, 2500);
+
+    };
+
+    const callResumeCoffee = () => {
+      console.log("playing outro");
+      this.play_btn.play(75, 0);
+    };
+
+
+   return(
+     <View style={GlobalStyles.backgroundColor}>
+       {/*<Text style={colors.text}>Hello there!</Text>*/}
+       {/*<Button title={"play music"} color={colors.button.backgroundColor} />*/}
+       {/*<Button title={"reset music"} color={colors.button.color} />*/}
+
+       {/*  * making lottie button animation */}
+       <View>
+         <TouchableWithoutFeedback onPress={playCoffee}>
+           <LottieView
+             ref={
+               animation => this.play_btn = animation
+             }
+             style={{ width: 100, height: 100 }}
+             source={require("../../assets/lottie/21451-coffee-and-love.json")}
+             loop={false} />
+         </TouchableWithoutFeedback>
+       </View>
+     </View>
+   )
   }
+}
 
-
-  return (
-    <View style={GlobalStyles.backgroundColor}>
-      <Text style={colors.text}>Hello there!</Text>
-      <Button title={"play music"} color={colors.button.backgroundColor} />
-      <Button title={"reset music"} color={colors.button.color} />
-      {/*<LottieView style={{ width: 500 }} source={require("../../assets/lottie/manLottie.json")} autoPlay={true} />*/}
-      {/*<LottieView style={{ width: 100, height: 100 }} source={require("../../assets/lottie/21451-coffee-and-love.json")}*/}
-      {/*            autoPlay={true} />*/}
-      {/*<LottieView style={{ width: 100, height: 100 }} source={require("../../assets/lottie/icons8-react-native.json")}*/}
-      {/*            autoPlay={true} />*/}
-
-
-      {/*  * making lottie button animation */}
-      <View>
-        <TouchableWithoutFeedback onPress={playCoffee}>
-          <LottieView
-            ref={
-              animation => this.play_btn = animation
-            }
-            style={{ width: 100, height: 100 }}
-            source={require("../../assets/lottie/21451-coffee-and-love.json")}
-            loop={false} />
-        </TouchableWithoutFeedback>
-      </View>
-    </View>
-  );
-};
 export default (Demo);
